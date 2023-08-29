a31663f995c4cfe577a11a354a9841dc62f1311a
Raul <raul.alhena@gmail.com>
2023-08-29 11:15:12 +0200

    setup pgsql & springboot

---
+++ campers/src/main/java/com/campers/campers/CampersApplication.java
@@ -0,0 +1,13 @@
+package com.campers.campers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampersApplication.class, args);
+	}
+
+}
